9d1ceef2637d335b33f7dc897eff1ac7eb92d115
Karolina <karolina16478@gmail.com>
2025-11-18 12:09:00 +0100

    Added new parameter to BoxDecoration Configuration file = BoxShape

--- lib/configuration_files/container_decoration.dart
+++ lib/configuration_files/container_decoration.dart
@@ -2,21 +2,23 @@ import 'package:flutter/material.dart';
 
 BoxDecoration containerDecoration({
   Color color = const Color(0xFF0B0720),
-  double borderRadius = 8.0,
+  double borderRadius = 0.0,
   bool showShadow = true,
-}){
+  BoxShape? shape,
+}) {
   return BoxDecoration(
     color: color,
-    borderRadius: BorderRadius.circular(borderRadius),
+    borderRadius: shape == null ? BorderRadius.circular(borderRadius) : null,
     boxShadow: showShadow
         ? [
-            BoxShadow(
-              color: Colors.grey,
-              spreadRadius: 2,
-              blurRadius: 5,
-              offset: const Offset(0, 3),
-            ),
-          ]
+      BoxShadow(
+        color: Colors.grey,
+        spreadRadius: 2,
+        blurRadius: 5,
+        offset: const Offset(0, 3),
+      ),
+    ]
         : null,
+    shape: shape ?? BoxShape.rectangle,
   );
 }
